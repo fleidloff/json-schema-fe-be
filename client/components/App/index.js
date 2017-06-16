f99a025e54eeb87e523cabc37ab10728afc93c16
Frederik Leidloff <fleidloff@team.mobile.de>
2017-06-17 01:26:13 +0200

    add persons list

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -6,6 +6,7 @@ import PersonList from "../Person/PersonList";
 
 export default () => (
   <div>
+    <a href="https://github.com/mozilla-services/react-jsonschema-form">https://github.com/mozilla-services/react-jsonschema-form</a>
     <PersonAdd />
     <PersonList />
   </div>
